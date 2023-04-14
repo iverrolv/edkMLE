c7420f4462e0f6c5503b69c624bcf80284233e34
Iver Rolvsjord <iverrolv@hotmail.com>
2023-04-14 13:09:36 +0200

    Fine tunign

--- ProjectMLE.m
+++ ProjectMLE.m
@@ -16,9 +16,10 @@ n0 = -P/N; % -256
 n = n0:1:n0+N-1;
 
 SNR = 10;
+SNRvec = -10:10:60;
 k = 20;
+kvec = 10:2:20;
 M = 2^k;
-m = 0:1:M-1;
 % White Gaussian noise, E=0, Var = Sigma2
 
 Num = 100;
@@ -35,7 +36,6 @@ for i = 1:Num
     % x[n]
     x = A*exp(1i*(w0*n*T+phi))+wgn;
     zero_padding_back = 513;
-    n_padded = 0:1:1023;
     xpad = [x zeros(1, zero_padding_back)];
     %plot(n, x)
     % CRLB for phase and freq:
@@ -54,10 +54,45 @@ end
 
 mean(phiBar)
 
+%% Fminsearch
+clc;
+k = 10;
+M = 2^k;
 
-function out = FF(w)
-    tmp = 0;
-    out = 1/N*tmp;
+% Consts
+Fs = 1e6;
+f0 = 1e5;
+T = 1e-6;
+w0 = 2*pi*f0;
+phi = pi/8;
+A = 1;
 
+% Samples
+N = 513;
+P = N*(N-1)/2;
+Q = N*(N-1)*(2*N-1)/6;
+n0 = -P/N; % -256
+n = n0:1:n0+N-1;
+
+SNR = 10;
+
+xFFT = fft(xpad, M);
+absxFFT = abs(xFFT);
+[mval, mstar] = max(abs(xFFT));
+wFFT = 2*pi/(M*T)*mstar;
+F = @(w) 1/N*sum(x.*exp(-1i*w*(0:1:N-1)*T));
+phiEst = angle(exp(-1i*wFFT*n0*T)*F(wFFT));
+    
+wFFT
+for i=0:0
+    wOpt = fminsearch(@(w)fun(w, absxFFT, n0, n, N, T, A, zero_padding_back, M, x), wFFT)
 end
 
+
+function mse = fun(w, absxFFT, n0, n, N, T, A, z, M, x)
+    F = @(w) 1/N*sum(x.*exp(-1i*w*(0:1:N-1)*T));
+    phiNew = angle(exp(-1i*w*n0*T)*F(w));
+    sFFT = abs(fft([A*sin(w*n+phiNew) zeros(1, z)], M));
+    error = absxFFT - sFFT;
+    mse = sqrt(mean((error).^2));
+end
